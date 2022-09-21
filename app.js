e6cbd9bbfdb8e9cd7e8eb996b74b9e0b7be7f587
Skulkrone <40754599+Skulkrone@users.noreply.github.com>
2022-09-21 15:42:47 +0200

    maj script

--- app.js
+++ app.js
@@ -1,7 +1,46 @@
-const btns = document.querySelectorAll('.btn-modale');
-const modaleEquipement = document.querySelector('.bloc-modale');
-const imgIndex = document.querySelector('.bloc-modale img');
+const btns = document.querySelectorAll(".btn-modale");
+const modaleEquipement = document.querySelector(".bloc-modale");
+const imgIndex = document.querySelector(".bloc-modale img");
 
+window.addEventListener("resize", () => {
+  responsiveModals();
+});
+
+function responsiveModals() {
+  if (window.matchMedia("(min-width:850px)").matches) {
+    btns.forEach((btn) => {
+      btn.addEventListener("click", clickBtn);
+    });
+
+    modaleEquipement.addEventListener("click", () => {
+      modaleEquipement.classList.remove("active-modale");
+    });
+  } else {
+    btns.forEach((btn) => {
+      btn.removeEventListener("click", clickBtn);
+    });
+  }
+}
+responsiveModals();
+
+function clickBtn(e) {
+  imgIndex.src = `ressources/img${e.target.getAttribute(
+    "data-index"
+  )}-equip.jpg`;
+  modaleEquipement.classList.add("active-modale");
+}
+
+// Anim navbar
+
+const nav = document.querySelector("nav");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 30) {
+    nav.classList.add("anim-nav");
+  } else {
+    nav.classList.remove("anim-nav");
+  }
+});
 
 // btns.forEach(btn => {
 //   btn.addEventListener('click', (e) => {
@@ -16,35 +55,20 @@ const imgIndex = document.querySelector('.bloc-modale img');
 //   modaleEquipement.classList.remove('active-modale');
 // })
 
-const mqLarge = window.matchMedia( '(min-width : 850px)' );
+// const mqLarge = window.matchMedia( '(min-width : 850px)' );
 // console.log( mqLarge.matches );
 
+// if(mqLarge.matches){
+//     btns.forEach(btn => {
+//     btn.addEventListener('click', (e) => {
 
-if(mqLarge.matches){
-    btns.forEach(btn => {
-    btn.addEventListener('click', (e) => {
+//         imgIndex.src = `ressources/img${e.target.getAttribute('data-index')}-equip.jpg`;
+//         modaleEquipement.classList.add('active-modale');
 
-        imgIndex.src = `ressources/img${e.target.getAttribute('data-index')}-equip.jpg`;
-        modaleEquipement.classList.add('active-modale');
+//     })
+//   })
 
-    })
-  })
-
-  modaleEquipement.addEventListener('click', () => {
-    modaleEquipement.classList.remove('active-modale');
-  })
-}
-
-// Anim navbar
-
-const nav = document.querySelector('nav');
-
-window.addEventListener('scroll', () => {
-
-    if(window.scrollY > 30) {
-        nav.classList.add('anim-nav');
-    } else {
-        nav.classList.remove('anim-nav');
-    }
-
-})
+//   modaleEquipement.addEventListener('click', () => {
+//     modaleEquipement.classList.remove('active-modale');
+//   })
+// }
